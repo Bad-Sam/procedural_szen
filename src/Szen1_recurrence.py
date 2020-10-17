c33e795946a1de22585d91791b1219a067c9466a
s.amara <s.amara@student.isartdigital.com>
2020-10-18 00:23:37 +0200

    Added a few comments, added assertions to check passed parameters

---
+++ src/Szen1_recurrence.py
@@ -0,0 +1,40 @@
+from complex_draw import draw_circle
+from PIL import Image, ImageDraw
+
+# Draw motif1 from a starting radius, and shrinking coefficient,
+# and a number of iterations, from the 
+# \param draw ImageDraw drawing context
+# \param R Radius of the first outer circle
+# \param q Shrinking coefficient applied to the radius of consecutive circles
+# \param N Number of circle/iterations to go through
+def Szen1_recurrence(draw : ImageDraw, R : float, q : float, N : int):
+    # Safety checks
+    assert (N > 2), "N must be superior to 2"
+    assert (q > 0 and q < 1), "q must be between 0 and 1 excluded"
+
+    # w1 and R1
+    center = radius = R
+
+    # Constant part pre-computed to simplify calculations in the loop 
+    constant_part = ((q ** -1) - 1) / (N - 2)
+
+    # Draw the first outer circle
+    draw_circle(draw, center + 500j, R)
+
+    # Find the center and radius of the next circles, and draw them
+    for i in range(1, N):
+        radius *= q
+        center += radius * constant_part * (2 * i - N)
+
+        draw_circle(draw, center + 500j, radius)
+
+
+# ====== Main ======
+# Initialize drawing context
+image = Image.new("RGB", (1000, 1000), color="#FFFFFF")
+draw = ImageDraw.Draw(image)
+
+Szen1_recurrence(draw, R=500, q=.75, N=6)
+
+image.show()
+# image.save("out.png")
