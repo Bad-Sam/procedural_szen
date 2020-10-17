d3578e9c10839e62e218f1eab978509b2bf58c07
s.amara <s.amara@student.isartdigital.com>
2020-10-18 00:26:55 +0200

    Implementation of pattern 1 with the explicit approach

--- src/Szen1_explicit.py
+++ src/Szen1_explicit.py
@@ -1,6 +1,12 @@
 from complex_draw import draw_circle
 from PIL import Image, ImageDraw
 
+# Draw pattern 1 from a starting radius, shrinking coefficient,
+# and a number of iterations, with the explicit approach
+# \param draw ImageDraw drawing context
+# \param R Radius of the first outer circle
+# \param q Shrinking coefficient applied to the radius of consecutive circles
+# \param N Number of circles/iterations to go through
 def Szen1_explicit(draw : ImageDraw, R : float, q : float, N : int):
     # Safety checks
     assert (N > 2), "N must be superior to 2"
