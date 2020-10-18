b7f6774c80b02d7e35ff7d112c6f26f41194a4f0
s.amara <s.amara@student.isartdigital.com>
2020-10-18 22:48:06 +0200

    Recurrence and explicit implementation of pattern 2

---
+++ src/Szen2_explicit.py
@@ -0,0 +1,60 @@
+from cmath import exp, pi
+from complex_draw import draw_circle, draw_cross
+from PIL import Image, ImageDraw  
+
+# Draw pattern 2 from a starting radius, shrinking coefficient,
+# and a number of iterations, with the explicit approach
+# \param draw ImageDraw drawing context
+# \param R Radius of the first outer circle
+# \param R Radius of the first outer circle
+# \param q Shrinking coefficient applied to the radius of consecutive circles
+# \param a Angle between the consecutive circles' radius lines
+# \param N Number of circles/iterations to go through
+# \param show_convergence Whether to put a cross on the point of convergence of the pattern
+def Szen2_explicit(draw : ImageDraw, R : float, q : float, a : float, N : int, show_convergence : bool = False):
+    # Safety checks
+    assert (N > 2), "N must be superior to 2"
+    assert (q > 0 and q < 1), "q must be between 0 and 1 excluded"
+
+    # Since we don't flip the image, the direct rotation is with
+    # a negative angle
+    a = -a
+
+    # Induction variables
+    qeian       = 1
+    radius      = R
+
+    # Constant parts pre-computed to simplify calculations in the loop
+    w1          = R + 500j
+    qeia        = q * exp(1j * a)
+    const_part  = R * qeia * ((q ** -1) - 1) / (1 - qeia)
+
+    # Save the first circle's computation (simpler this way)
+    draw_circle(draw, R + 500j, R)
+
+    for n in range(2, N + 1):
+        # Update the induction variable
+        qeian   *= qeia
+        radius  *= q
+        
+        wn      = w1 - const_part * (1 - qeian)
+        
+        draw_circle(draw, wn, radius)
+
+    if show_convergence:
+        factor = exp(1j * a)
+        draw_cross(draw, w1 - (R * q * factor * ((q ** -1) - 1)) / (1 - q * factor))
+
+
+# ====== Main ======
+# Initialize drawing context
+image = Image.new("RGB", (1000, 1000), color="#FFFFFF")
+draw = ImageDraw.Draw(image)
+
+Szen2_explicit(draw, R=500, q=.85, a=pi / 4, N=6)
+
+# With convergence point pin-pointed
+#Szen2_explicit(draw, R=500, q=.85, a=pi / 4, N=6, show_convergence=True)
+
+image.show()
+# image.save("out.png")
---
+++ src/Szen2_recurrence.py
@@ -0,0 +1,58 @@
+from cmath import exp, pi
+from complex_draw import draw_circle, draw_cross
+from PIL import Image, ImageDraw
+
+# Draw pattern 2 from a starting radius, shrinking coefficient, rotation angle,
+# and a number of iterations, with the recurrence approach
+# \param draw ImageDraw drawing context
+# \param R Radius of the first outer circle
+# \param q Shrinking coefficient applied to the radius of consecutive circles
+# \param a Angle between the consecutive circles' radius lines
+# \param N Number of circles/iterations to go through
+# \param show_convergence Whether to put a cross on the point of convergence of the pattern
+def Szen2_recurrence(draw : ImageDraw, R : float, q : float, a : float, N : int, show_convergence : bool = False):
+    # Safety checks
+    assert (N > 2), "N must be superior to 2"
+    assert (q > 0 and q < 1), "q must be between 0 and 1 excluded"
+
+    # Since we don't flip the image, the direct rotation is with
+    # a negative angle
+    a = -a
+
+    # Induction variables
+    # w1 and R1
+    center = R + 500j
+    radius = R    
+    e = 1
+
+    # Constant part pre-computed to simplify calculations in the loop 
+    constant_part   = ((q ** -1) - 1)
+    factor          = exp(1j * a)
+
+    # Draw the first outer circle
+    draw_circle(draw, center, R)
+
+    # Find the center and radius of the next circles, and draw them
+    for n in range(1, N):
+        e *= factor
+        radius *= q
+        center -= radius * constant_part * e
+
+        draw_circle(draw, center, radius)
+
+    if show_convergence:
+        draw_cross(draw, (R + 500j) - (R * q * factor * constant_part) / (1 - q * factor))
+
+
+# ====== Main ======
+# Initialize drawing context
+image = Image.new("RGB", (1000, 1000), color="#FFFFFF")
+draw = ImageDraw.Draw(image)
+
+Szen2_recurrence(draw, R=500, q=.85, a=pi / 3, N=9)
+
+# With convergence point pin-pointed
+#Szen2_recurrence(draw, R=500, q=.85, a=pi / 4, N=6, show_convergence=True)
+
+image.save("R=500_q=0.85_a=pidiv3_N=9.png")
+# image.save("out.png")
